12ca786bd747455f0d2eab91fe9652be31e77099
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2022-10-16 10:17:58 -0400

    Update Redux with the newest toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NewNoteInput } from './NewNoteInput';
-import { NoteState } from './notesReducer';
-
+import { noteSlice, NoteState } from './redux/notesReducer';
+import { addNoteREDUX, selectNotes } from './redux/notesReducer';
 
 
 function App() {
-  const notes = useSelector<NoteState, NoteState["notes"]>((state) => state.notes);
+  const notes = useSelector(selectNotes);
 
   const dispatch = useDispatch();
 
   const addNote = (note: string) => {
-    dispatch({type: "ADD_NOTE", payload: note})
+    dispatch(addNoteREDUX(note))
   };
 
   return (

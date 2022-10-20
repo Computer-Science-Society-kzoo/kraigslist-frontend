6828ae3b99adf7d032162605a85d7492f5da8b84
Derik Torres <deriktorres33@gmail.com>
2022-10-20 11:48:41 -0400

    Account login/signup inputs

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,6 +2,24 @@ export function Login(): JSX.Element {
   return(
     <div>
       <h1>Login page</h1>
+
+      <form>
+          <div className="class-container">
+            <label>
+               Username 
+              <input type="text" name="uname" required/>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Password 
+              <input type="text" name="pword" required/>
+            </label>
+          </div>
+        </form>
+        
     </div>
+
   )
 }
--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -2,6 +2,49 @@ export function Signup(): JSX.Element {
     return(
       <div>
         <h1>Signup page</h1>
+        <form>
+          <div className="class-container">
+            <label>
+               Email 
+              <input type="text" name="email" required/>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              First Name 
+              <input type="text" name="fName" required/>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Last Name 
+              <input type="text" name="lName" required/>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Prefered Name 
+              <input type="text" name="pName" required/>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Year 
+              <input type="text" name="fName" required/>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Password 
+              <input type="text" name="pword" required/>
+            </label>
+          </div>
+        </form>
       </div>
     )
   }

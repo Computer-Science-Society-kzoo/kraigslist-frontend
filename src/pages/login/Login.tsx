0ac18ba5323b99de81e37b3750c6ded9c57a8729
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2022-10-20 20:57:00 -0400

    Handle login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -5,19 +5,20 @@ import {
   FormHelperText,
   Input,
   Button,
-  ButtonGroup
-} from '@chakra-ui/react'
+  ButtonGroup,
+  useToast,
+} from "@chakra-ui/react";
 
-import axios from 'axios'
-import { useState } from 'react'
+import axios from "axios";
+import e from "express";
+import { useState } from "react";
 
-import './Login.css'
+import "./Login.css";
 
 export function Login(): JSX.Element {
-
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
+  const toast = useToast();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   // function test(value: string){
   //   setEmail(value)
@@ -26,47 +27,98 @@ export function Login(): JSX.Element {
 
   async function login(email: string, password: string) {
     //alert("Email is: " + email + " Password is: " + password)
-    const json = JSON.stringify({ email: email, password: password })
-    axios.post('http://localhost:3000/api/auth/login', { email: email, password: password })
-    .then((res) => {
-      console.log(res)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+    if (email === "") {
+      toast({
+        title: "Missing information.",
+        description: "Please provide your email address.",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    } else if (password === "") {
+      toast({
+        title: "Missing information.",
+        description: "Please provide your password.",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    } else {
+      axios
+        .post("http://localhost:3000/api/auth/login", {
+          email: email,
+          password: password,
+        })
+        .then((res) => {
+          console.log(res);
+          toast({
+            title: "Success!",
+            description:
+              "You have successfully logged in. Your token: " + res.data,
+            status: "success",
+            duration: 9000,
+            isClosable: true,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          if (err.response.status === 401) {
+            toast({
+              title: "Incorrect login information.",
+              description: "Please check your email and password.",
+              status: "error",
+              duration: 9000,
+              isClosable: true,
+            });
+          } else if (err.response.status === 404) {
+            toast({
+              title: "Error",
+              description: "The user does not exist.",
+              status: "error",
+              duration: 9000,
+              isClosable: true,
+            });
+          }
+        });
+    }
   }
 
-  return(
+  return (
     <div>
       <h1>Login page</h1>
 
-      <div className='LoginSignupForm'>
-          <FormControl isRequired>
-            <FormLabel> Email </FormLabel>
-            <Input onChange={(el) => setEmail(el.target.value)} placeholder="Email" required/>
-          </FormControl>
-          
+      <div className="LoginSignupForm">
+        <FormControl isRequired>
+          <FormLabel> Email </FormLabel>
+          <Input
+            onChange={(el) => setEmail(el.target.value)}
+            placeholder="Email"
+            required
+          />
+        </FormControl>
 
-          <FormControl isRequired>
-            <FormLabel> Password </FormLabel>
-            <Input type={"password"} onChange={(el) => setPassword(el.target.value)} placeholder="Password" required/>
-          </FormControl>
+        <FormControl isRequired>
+          <FormLabel> Password </FormLabel>
+          <Input
+            type={"password"}
+            onChange={(el) => setPassword(el.target.value)}
+            placeholder="Password"
+            required
+          />
+        </FormControl>
 
-          <FormControl>
-            <Button 
-              colorScheme='orange'
-              type='submit'
-              variant='solid'
-              loadingText='Logging In'
-              onClick={() => login(email, password)}
-            >
-              Login
-            </Button>
-          </FormControl>
-
-        </div>
-        
+        <FormControl>
+          <Button
+            colorScheme="orange"
+            type="submit"
+            variant="solid"
+            loadingText="Logging In"
+            onClick={() => login(email, password)}
+          >
+            Login
+          </Button>
+        </FormControl>
+      </div>
     </div>
-
-  )
+  );
 }

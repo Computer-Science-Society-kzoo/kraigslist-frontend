01e38d67daad19647ade54269005013bc54e9680
Derik Torres <deriktorres33@gmail.com>
2022-11-04 15:24:40 -0400

    Simple Profile Page

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,21 +1,27 @@
 import { Heading, InputGroup, IconButton, Input } from "@chakra-ui/react";
-
 import { SearchIcon } from "@chakra-ui/icons";
 
 export function SearchBar(): JSX.Element {
   return (
-    <div className="search.css">
+    <div>
       <Heading as="h2" size="xs" variant="outlined">
-        <InputGroup size="sm">
+        <InputGroup size="sm"
+        width={768}
+        outline="1px solid #F6AD55"
+        borderRadius={"10px 10px 10px 10px"}
+        >
           <IconButton 
           aria-label="Search database" 
           icon={<SearchIcon />} 
           borderRadius={"10px 0px 0px 10px"}
           colorScheme="orange" 
           />
-          <Input placeholder="Search" 
+          <Input 
+          placeholder="Search" 
           borderRadius={"0px 10px 10px 0px"}
+          
           focusBorderColor="orange.500"
+          _placeholder={{ color: 'orange.500' }}
           />
         </InputGroup>
       </Heading>
--- src/pages/main/Profile.tsx
+++ src/pages/main/Profile.tsx
@@ -1,44 +1,135 @@
-import { EditIcon } from "@chakra-ui/icons";
-import { 
-    FormControl,
-        FormLabel,
-        Heading,
-        Input,
-        InputGroup,
-        InputRightAddon,
-        Stack,
-        } from "@chakra-ui/react";
+import { EditIcon, CheckIcon, CloseIcon } from "@chakra-ui/icons";
+import {
+  FormControl,
+  FormLabel,
+  Heading,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightAddon,
+  Stack,
+  Editable,
+  EditableInput,
+  EditablePreview,
+  useEditableControls,
+  ButtonGroup,
+  Button,
+  useToast,
+} from "@chakra-ui/react";
+import { useCookies } from "react-cookie";
 import { Form } from "react-router-dom";
+import { setAuthRedux } from "../../redux/coreReducer";
+import "./Profile.css";
 
 export function Profile(): JSX.Element {
-    return(
-        <div>
-            <Header />
-            <ProfileInfo/>
-        </div>
-    );
+
+  return (
+
+    <div>
+      <ProfileInfo />
+    </div>
+  );
 }
+
 
 function Header(): JSX.Element {
-    return (
-        <Heading as='h2' size='md'>
-            Profile Settings
-        </Heading>
-    )
+  return (
+    <Heading as="h2" size="md" className="Profile-Header">
+      Your Profile
+    </Heading>
+  );
 }
 
+
+function EditControl() {
+  const {
+    isEditing,
+    getSubmitButtonProps,
+    getCancelButtonProps,
+    getEditButtonProps,
+  } = useEditableControls();
+
+  return isEditing ? (
+    <ButtonGroup justifyContent="end" size="sm" w="full">
+      <IconButton
+        aria-label="check"
+        icon={<CheckIcon boxSize={3} />}
+        {...getSubmitButtonProps()}
+      />
+      <IconButton
+        aria-label="close"
+        icon={<CloseIcon boxSize={3} />}
+        {...getCancelButtonProps()}
+      />
+    </ButtonGroup>
+  ) : (
+    <IconButton
+      aria-label="edit"
+      icon={<EditIcon />}
+      {...getEditButtonProps()}
+    />
+  );
+}
+
+
+
 function ProfileInfo(): JSX.Element {
-    return (
-        <div>
-            <Stack>
-                <FormControl isDisabled={true}>
-                    <FormLabel>Name</FormLabel>
-                    <InputGroup>
-                        <Input placeholder="First Name" />
-                        <InputRightAddon children={<EditIcon/>}/>
-                    </InputGroup>
-                </FormControl>
-            </Stack>
-        </div>
-    )
+
+  const [token, setToken, removeToken] = useCookies(["auth"]);
+  const toast = useToast();
+
+  function testSignOut() {
+    setAuthRedux(false);
+    removeToken("auth");
+    toast({
+      title: "Warning",
+      description: "You are now logged out.",
+      status: "warning",
+      duration: 9000,
+      isClosable: true,
+    });
+  }
+
+  return (
+
+    <div className="Profile-Container">
+      
+      <Header />
+      <Stack>
+        <Editable defaultValue="Name" isPreviewFocusable={false}>
+          <EditablePreview />
+          <Input as={EditableInput} />
+          <EditControl />
+        </Editable>
+
+        <Editable defaultValue="Pronouns" isPreviewFocusable={false}>
+          <EditablePreview />
+          <Input as={EditableInput} />
+          <EditControl />
+        </Editable>
+
+        <Editable defaultValue="Username" isPreviewFocusable={false}>
+          <EditablePreview />
+          <Input as={EditableInput} />
+          <EditControl />
+        </Editable>
+
+        <Editable defaultValue="Email" isPreviewFocusable={false}>
+          <EditablePreview />
+          <Input as={EditableInput} />
+          <EditControl />
+        </Editable>
+
+        <Editable defaultValue="Year" isPreviewFocusable={false}>
+          <EditablePreview />
+          <Input as={EditableInput} />
+          <EditControl />
+        </Editable>
+
+        <Button onClick={testSignOut} colorScheme="orange">
+          Sign Out
+        </Button>
+      </Stack>
+    </div>
+  );
 }

dfa5179557e1a4e7824e2297ba8e5720a8a4de94
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2022-11-05 22:57:14 -0400

    Connect Messages

--- src/pages/messages/MessagesPage.tsx
+++ src/pages/messages/MessagesPage.tsx
@@ -1,5 +1,6 @@
 import "./MessagesPage.css";
-import { Divider, Text, Heading, Avatar } from "@chakra-ui/react";
+import { Divider, Text, Heading, Avatar, Input } from "@chakra-ui/react";
+import { ChatIcon } from "@chakra-ui/icons";
 import { useCookies } from "react-cookie";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -12,7 +13,14 @@ function Conversation(): JSX.Element {
     )
 }
 
+interface Converstaion {
+  comID: number;
+  name: string;
+  lastMessage: string;
+}
+
 function ConversationItem(props: Converstaion): JSX.Element {
+
     return (
         <div className="ConversationItem">
               <Avatar size='lg' name={props.name}/>
@@ -26,21 +34,94 @@ function ConversationItem(props: Converstaion): JSX.Element {
     )
 }
 
-interface Converstaion {
-    name: string;
-    lastMessage: string;
+interface Message {
+  message: string;
 }
+
+function Message(props: Message): JSX.Element {
+    return (
+        <div className="MessageItem">
+            <Text className="imessage" fontSize='md'>{props.message}</Text>
+        </div>
+    )
+}
+
+function MessageContainer(props: {id: number}): JSX.Element {
+
+  const [token, setToken, removeToken] = useCookies(["auth"]);
+
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  function parseMessages(newMessages: any) {
+    let parsedMessages: Message[] = [];
+    newMessages.forEach((con: any) => {
+      parsedMessages.push({
+        message: con.message,
+      });
+    });
+    return parsedMessages;
+  }
+
+  async function getMessages() {
+    console.log(token.auth);
+    axios
+      .get("http://localhost:3000/api/messages/allmessages", { 
+        "headers":
+        {
+          "Authorization": `Bearer ${token.auth}`,
+          "Comradeid": props.id,
+        }
+      })
+      .then((res) => {
+        console.log(res.data);
+        setMessages(parseMessages(res.data));
+      });
+  }
+
+
+    useEffect(() => {
+      getMessages();
+    }, [props])
+
+    return (
+        <div className="MessageContainer">
+            {props.id === -1 && 
+              <div className="MessageContainer-NoSelection">
+                <ChatIcon/>
+                <Text fontSize='xl' noOfLines={3}>
+                  Select a conversation to start messaging
+                </Text>
+              </div>
+            }
+            { props.id !== -1 &&
+              <div className="MessageContainer-Selected">
+                <Heading as='h1' size='lg'>
+                  {props.id}
+                </Heading>
+                {messages.map((message) => (
+                  <Message message={message.message}/>
+                ))}
+
+                <Input placeholder='Your message...' />
+              </div>
+            }
+        </div>
+    )
+}
+
 
 export function MessagesPage(): JSX.Element {
   
   const [token, setToken, removeToken] = useCookies(["auth"]);
 
   const [conversations, setConversations] = useState<Converstaion[]>([]);
+  const [selectedConversation, setSelectedConversation] = useState<number>(-1);
 
   function parsePosts(newConversations: any) {
     let parsedConversations: Converstaion[] = [];
     newConversations.forEach((con: any) => {
       parsedConversations.push({
+        comID: con.comID,
         name: con.name,
         lastMessage: con.lastMessage,
       });
@@ -72,13 +153,13 @@ export function MessagesPage(): JSX.Element {
       <div className="MessagesPageContainer-Main">
         <div className="ConversationContainer">
           {conversations.map((con) => (
-            <ConversationItem name={con.name} lastMessage={con.lastMessage} />
+            <a onClick={() => setSelectedConversation(con.comID) }>
+              <ConversationItem comID={con.comID} name={con.name} lastMessage={con.lastMessage} />
+            </a>
           ))}
         </div>
         <Divider orientation='vertical'/>
-        <div>
-          hello 2
-        </div>
+          <MessageContainer id={selectedConversation}/>
       </div>
     </div>
   );

c5b15379f92ef1fca4302e6279d8c07a4625d12f
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2022-11-06 19:12:24 -0500

    Update Guidelines.tsx

--- src/pages/main/Guidelines.tsx
+++ src/pages/main/Guidelines.tsx
@@ -15,7 +15,7 @@ function FunnyGuidelines() {
         <div>
         <p>
         In an effort to maintain a respectful and civil user environment, we ask that all users refrain from posting any content that could be considered offensive towards Dr. Vargas-Perez.
-        This includes, but is not limited to, posting his bad jokes, personal attacks on using Sublime, or any other content that could be considered disrespectful. Kraigslist believes that
+        This includes, but is not limited to, posting his bad jokes (Aleksandr: can we change this to "posting his jokes without his permission" i've never heard of bad jokes from Dr. Vargas-Perez), personal attacks on using Sublime, or any other content that could be considered disrespectful. Kraigslist believes that
         maintaining a respectful community is essential to the success of Kragislist, and we thank you for your cooperation.
         </p>
         </div>

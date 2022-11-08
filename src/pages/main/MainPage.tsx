d1ece902683d357fa7791df9cead9a878e2b04f2
hanissommer <hanissommerville@gmail.com>
2022-11-08 14:20:21 -0500

    Added search functionality

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,4 +1,4 @@
-import { Container, Heading, Text } from "@chakra-ui/react";
+import { Container, Heading, IconButton, Input, InputGroup, Text } from "@chakra-ui/react";
 import "./Post.css";
 import "./MainPage.css";
 import axios from "axios";
@@ -9,6 +9,7 @@ import Split from 'react-split'
 import './Split.css';
 import { MakePost } from "./makePost";
 import { SearchBar } from "../../components/SearchBar";
+import { SearchIcon } from "@chakra-ui/icons";
 
 interface Post {
   title: string;
@@ -81,7 +82,59 @@ export function MainPage(): JSX.Element {
   }
 
   useEffect(() => {
-    getPosts();
+    if (text != "") {
+      search(text);
+    }
+    else {
+      getPosts();
+    }
+  }, []);
+
+  // async function getsearchPosts(text: string) {
+  //   console.log(token.auth);
+  //   axios
+  //     .get("http://localhost:3000/api/searchPosts", { params: { text: text } })
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       setPosts(parsePosts(res.data));
+  //     });
+  // }
+
+  const [text, setText] = useState("");
+
+
+  async function search(text: string) {
+
+    console.log(text);
+    if (text === "") {
+      return
+      //messageFailure("Missing infomration", "Please provide a text to search.");
+    } else {
+      axios
+        .get("http://localhost:3000/api/posts/searchPosts", {
+          params: { text: text }
+        },
+
+        )
+        .then((res) => {
+          console.log(res.data);
+          setPosts(parsePosts(res.data));
+
+        })
+        .catch((err) => {
+          console.log(err);
+        }
+        );
+    }
+  }
+
+  useEffect(() => {
+    if (text != "") {
+      search(text);
+    }
+    else {
+      getPosts();
+    }
   }, []);
 
   const dummyPost: Post = {
@@ -99,10 +152,33 @@ export function MainPage(): JSX.Element {
     <Split className="split MainPageContainer" sizes={[20, 80]} maxSize={[500, Infinity]} minSize={[240, 500]} expandToMin={false}>
       <FiltersMenu />
       <div className="MainPageContainer-PostsContainer">
-         
-        <SearchBar/>
+        <div>
+          <Heading as="h2" size="xs" variant="outlined">
+            <InputGroup size="sm"
+              width={768}
+              outline="1px solid #F6AD55"
+              borderRadius={"10px 10px 10px 10px"}
+            >
+              <IconButton
+                aria-label="Search database"
+                icon={<SearchIcon />}
+                borderRadius={"10px 0px 0px 10px"}
+                colorScheme="orange"
+                onClick={() => search(text)}
+              />
+              <Input
+                placeholder="Search"
+                borderRadius={"0px 10px 10px 0px"}
+                onChange={(e) => setText(e.target.value)}
+                focusBorderColor="orange.500"
+                _placeholder={{ color: 'orange.500' }}
+              />
+            </InputGroup>
+          </Heading>
+        </div>
 
-          {posts.map((post) => (
+
+        {posts.map((post) => (
           <Post
             title={post.title}
             username={post.username}

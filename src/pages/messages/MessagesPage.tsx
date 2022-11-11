04d703ef835527f6df8ac871c4f676a99f90f43c
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2022-11-11 01:19:25 -0500

    Small fix

--- src/pages/messages/MessagesPage.tsx
+++ src/pages/messages/MessagesPage.tsx
@@ -272,6 +272,7 @@ function MessageContainer(props: conversationWithUpdateFunction): JSX.Element {
                                 key={counter++}
                                 initial={{ opacity: 0 }}
                                 animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
                                 transition={{ duration: 0.2}}
                               >
                                 {showFirstDate && 

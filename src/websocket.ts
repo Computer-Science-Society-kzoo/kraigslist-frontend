3b73ef5e4aa047d40af56999b2c7045138343d41
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2022-11-11 01:08:03 -0500

    UpdateWebSocket

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,5 +1,7 @@
+import { setActiveConversationRedux, setConversationsRedux } from "./redux/messagesReducer";
+import { useDispatch } from "react-redux";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
-
+import { storeRef } from "./redux/store";
 
 
 // When the connection is closed, print some data to the console.
@@ -8,7 +10,6 @@ const client = new W3CWebSocket('ws://localhost:8000')
 export function createWebSocketConnection() {
     return client;
 }
-
 
 // When the connection is open, print some data to the console.
 client.onopen = () => {
@@ -28,11 +29,19 @@ interface Message {
     data: any;
 }
 
+
 client.onmessage = (message) => {
+
+
     const dataFromServer: Message = JSON.parse(message.data?.toString());
     switch (dataFromServer.type) {
         case "newMessage":
             console.log("newMessage: ", message.data);
+            const data = {
+                conID: dataFromServer.data.conversationID,
+                message: dataFromServer.data.message
+            }
+            storeRef.dispatch(setActiveConversationRedux(data));
             break
         default:
             console.log("Undefined message type: ", message.data);
